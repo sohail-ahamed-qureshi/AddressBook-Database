ffbc93ee97ba69097d1f6332609b23aaee8dd594
sohail <sohailqureshi82@gmail.com>
2021-07-01 12:27:01 +0530

    [sohail] Add: creating new table of contacts

--- AddressBookDatabase/SQLQuery.sql
+++ AddressBookDatabase/SQLQuery.sql
@@ -1,3 +1,13 @@
 ﻿Create Database AddressBook;
 use AddressBook;
 
+create table Contacts(
+firstName varchar(255),
+lastName varchar(255),
+address varchar(255),
+city varchar(255),
+state varchar(255),
+zip int,
+phoneNumber bigint,
+email varchar(255)
+);
