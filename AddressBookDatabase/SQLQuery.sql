866df4d656ff7cc96ad1c3caf41f4778e58925c2
sohail <sohailqureshi82@gmail.com>
2021-07-01 12:49:35 +0530

    [sohail] Add: ability to edit existing contact using name

--- AddressBookDatabase/SQLQuery.sql
+++ AddressBookDatabase/SQLQuery.sql
@@ -1,8 +1,8 @@
-﻿--UC1
+﻿--UC1 -- create new database for addressbook
 Create Database AddressBook;
 use AddressBook;
 
---UC2
+--UC2 -- create new table for address book
 create table Contacts(
 firstName varchar(255),
 lastName varchar(255),
@@ -14,9 +14,15 @@ phoneNumber bigint,
 email varchar(255)
 );
 
---UC3
+--UC3 -- ability to insert new data to table
 insert into contacts
 values('emily', 'watson', '#54 backstreet', 'NY', 'America', 123654, 09876554321, 'emily@email.com'),
 ('Dwayne', 'johnson', '#12 SM nagar', 'Mysore', 'Karnataka', 100004, 09876554321, 'johnson@email.com'),
 ('terisa', 'loui', '#3 AB nagar', 'Noida', 'UP', 543201, 09876554321, 'loui@email.com'),
 ('grey', 'moron', '#84 MJ nagar', 'NJ', 'London', 129123, 09876554321, 'grey@email.com');
+
+--UC4 -ability to edit existing contact using their name
+Update contacts set phoneNumber =  12345567809 where firstName = 'terisa';
+
+--display table
+select * from contacts
